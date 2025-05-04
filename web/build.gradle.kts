4d51c367660a652314e226c3ea5d0a3b7c002437
orekyuu <orekyuu@gmail.com>
2025-05-04 15:26:52 +0900

    Add Spring Boot framework integration

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     id("net.orekyuu.fansite.java-base")
+    alias(libs.plugins.spring)
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(libs.spring.starter.web)
+    testImplementation(libs.spring.starter.test)
 }
--- web/src/main/java/net/orekyuu/fansite/web/FansitePlatform.java
+++ web/src/main/java/net/orekyuu/fansite/web/FansitePlatform.java
@@ -1,7 +1,11 @@
 package net.orekyuu.fansite.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FansitePlatform {
   public static void main(String[] args) {
-
+    SpringApplication.run(FansitePlatform.class, args);
   }
 }
