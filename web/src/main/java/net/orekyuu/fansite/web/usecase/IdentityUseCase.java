2ac031f1acf4ec1d0442e74ab15e25827a255915
orekyuu <orekyuu@gmail.com>
2025-05-04 22:15:53 +0900

    Add logging for account registration validation failures

--- web/src/main/java/net/orekyuu/fansite/web/usecase/IdentityUseCase.java
+++ web/src/main/java/net/orekyuu/fansite/web/usecase/IdentityUseCase.java
@@ -5,12 +5,15 @@ import net.orekyuu.fansite.domain.identity.AccountId;
 import net.orekyuu.fansite.domain.identity.AccountRegistrationRule;
 import net.orekyuu.fansite.domain.identity.AccountRepository;
 import net.orekyuu.fansite.domain.validation.ValidationResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public record IdentityUseCase(
         AccountRepository accountRepository,
         TimeProducer timeProducer) {
+  private static final Logger logger = LoggerFactory.getLogger(IdentityUseCase.class);
 
   public static class AccountAlreadyRegisteredException extends Exception { }
 
@@ -18,6 +21,7 @@ public record IdentityUseCase(
     var rule = new AccountRegistrationRule(accountRepository, timeProducer);
     var canRegistration = switch (rule.apply(new AccountId(id), email)) {
       case ValidationResult.Failure(var reason, var message) -> {
+        logger.debug("ValidationResult is Failure. reason = {}, message = {}", reason, message);
         throw new AccountAlreadyRegisteredException();
       }
       case ValidationResult.Success(var canAccountRegistration) -> canAccountRegistration;
